09887cb0a9e2ed1cd1554bb173b9eb19f46aca68
Metaphysicist1 <edgarabasov1@gmail.com>
2025-01-28 00:07:06 +0400

    struct

---
+++ database/db.py
@@ -0,0 +1,9 @@
+# Import the Pinecone library
+from pinecone import Pinecone
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+# Initialize a Pinecone client with your API key
+pc = Pinecone(api_key=os.getenv("PINECONE_KEY"))
