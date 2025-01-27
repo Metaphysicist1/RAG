3363b8850af5fbcf92de9fb5a7361c0ed23d67f8
Metaphysicist1 <edgarabasov1@gmail.com>
2025-01-28 01:17:53 +0400

    set up

--- database/db.py
+++ database/db.py
@@ -1,5 +1,5 @@
 # Import the Pinecone library
-from pinecone import Pinecone
+from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv
 import os
 
@@ -7,3 +7,6 @@ import os
 load_dotenv()
 # Initialize a Pinecone client with your API key
 pc = Pinecone(api_key=os.getenv("PINECONE_KEY"))
+
+
+
